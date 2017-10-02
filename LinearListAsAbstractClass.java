f3b88ccbc1989c0a6af80e435f2e5507f86ebca4
ridwansyaifizan <syaifizanridwan@gmail.com>
2017-10-03 06:22:18 +0700

    Add files via upload

---
+++ LinearListAsAbstractClass.java
@@ -0,0 +1,11 @@
+package no2sampai5;
+
+public abstract class LinearListAsAbstractClass {
+    public abstract boolean isEmpty();
+    public abstract int size();
+    public abstract Object get(int index);
+    public abstract int indexOf(Object elem);
+    public abstract Object remove(int index);
+    public abstract void add(int indes, Object obj);
+    public abstract String toString();
+}
